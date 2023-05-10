0a6fb4715e15056d44b790c246e2bd4a16c06474
HanJin <2020253046@yonsei.ac.kr>
2023-05-10 16:49:23 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -1,17 +1,18 @@
-N, M = map(int, input().split())
-nums = sorted(map(int, input().split()))
-visited = [False] * N
-
-
-def backtracking(arr):
-    if len(arr) == M:
-        print(*arr)
-
-    for i in range(N):
-        if not visited[i]:
-            visited[i] = True
-            backtracking(arr + [nums[i]])
-            visited[i] = False
-
-
-backtracking([])
+for t in range(int(input())):
+    n = int(input())
+    arr = [list(map(int, input().split())), list(map(int, input().split()))]
+    dp = [[0] * n, [0] * n]
+    dp[0][0] = arr[0][0]
+    dp[1][0] = arr[1][0]
+    for i in range(1, n):
+        dp[0][i] = max(
+            dp[1][i - 1] + arr[0][i],
+            dp[0][i - 2] + arr[0][i] if i > 1 else -1,
+            dp[1][i - 2] + arr[0][i] if i > 1 else -1,
+        )
+        dp[1][i] = max(
+            dp[0][i - 1] + arr[1][i],
+            dp[1][i - 2] + arr[1][i] if i > 1 else -1,
+            dp[0][i - 2] + arr[1][i] if i > 1 else -1,
+        )
+    print(max(max(dp[0]), max(dp[1])))
