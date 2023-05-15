b45fa8b4fe7c1d30b3f4ddeaaf2d38f247eaf85e
Hanjin <happyleehan@gmail.com>
2023-05-16 02:26:27 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 208 ms, Memory: 123444 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+N, K = map(int, input().split())
+prev = [-1] * 100001
+
+q = deque([(N, 0)])
+while q:
+    x, c = q.popleft()
+    if x == K:
+        break
+    for nx in [x-1, x+1, 2*x]:
+        if 0 <= nx < 100001 and prev[nx] == -1:
+            prev[nx] = x
+            q.append((nx, c + 1))
+
+print(c)
+
+arr = []
+i = K
+while i != N:
+    arr.append(i)
+    i = prev[i]
+arr.append(N)
+
+for i in range(len(arr)-1, -1, -1):
+    print(arr[i], end=' ')
