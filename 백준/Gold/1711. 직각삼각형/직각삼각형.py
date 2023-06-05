6d0a77e640bc51a1992eb40c90ba0f2e25e55985
Hanjin <happyleehan@gmail.com>
2023-06-05 18:02:36 +0900

    [Gold V] Title: 직각삼각형, Time: 13412 ms, Memory: 116360 KB -BaekjoonHub

---
+++ 백준/Gold/1711. 직각삼각형/직각삼각형.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+points = [tuple(map(int, input().split())) for _ in range(N)]
+ans = 0
+for i in range(N - 2):
+    for j in range(i + 1, N - 1):
+        for k in range(j + 1, N):
+            p1, p2, p3 = points[i], points[j], points[k]
+            d1 = (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1])
+            d2 = (p2[0] - p3[0]) * (p2[0] - p3[0]) + (p2[1] - p3[1]) * (p2[1] - p3[1])
+            d3 = (p3[0] - p1[0]) * (p3[0] - p1[0]) + (p3[1] - p1[1]) * (p3[1] - p1[1])
+            if 2 * max(d1, d2, d3) == d1 + d2 + d3:
+                ans += 1
+print(ans)
